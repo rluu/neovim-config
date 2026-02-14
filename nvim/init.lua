1d45712a03d8455c6621fe2f139c745a2c0de3ba
Ryan Luu <ryanluu@gmail.com>
2026-02-14 02:34:13 +0000

    Make it so that the ENTER key does not accept an auto-complete suggestion, which was happening at the end of lines when I just wanted to go to the next line.  Instead, make the TAB key accept a suggestion.

--- nvim/init.lua
+++ nvim/init.lua
@@ -327,10 +327,15 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.abort()
+      end
+      fallback()
+    end, { 'i', 's' }),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_next_item()
+        cmp.confirm({ select = true })
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
       else
